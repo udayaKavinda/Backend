fcc749042c95c3e66b4eccc4c7b92365c0250332
udayaKavinda <e19155@eng.pdn.ac.lk>
2024-07-04 21:55:41 +0530

    backend

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Simple GET route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Simple POST route
+app.post('/data', (req, res) => {
+  const data = req.body;
+  res.send(`You sent: ${JSON.stringify(data)}`);
+});
+
+// Simple route with a URL parameter
+app.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`User ID is: ${userId}`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
